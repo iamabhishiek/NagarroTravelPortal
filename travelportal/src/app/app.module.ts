7ca628149e3339b21ba7c2b485fc295f73991586
Abhishiek Mahajan <abhishiek.mahajan@nagarro.com>
2020-04-25 19:54:33 +0530

    Add basic front end

--- travelportal/src/app/app.module.ts
+++ travelportal/src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ConfirmregistrationComponent } from './confirmregistration/confirmregistration.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RegistrationComponent,
+    ForgotPasswordComponent,
+    ConfirmregistrationComponent,
+    HomePageComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
